a5c69bb0f17d2b7d525ee8a497f857feb3fe5acb
jusjasjaz <jazminedimagiba3@gmail.com>
2024-04-29 23:02:20 +0800

    update form field

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -2,14 +2,14 @@ import { View, Text, TextInput, Image, TouchableOpacity } from 'react-native'
 import React,{ useState } from 'react'
 import { icons } from "../constants";
 
-const FormField = ({title, value, placeholder, handleChangeText, otherStyles, amountInput, ...props}) => {
+const FormField = ({title, value, placeholder, handleChangeText, otherStyles, ...props}) => {
   const [showPassword, setShowPassword] = useState(false)
 
   return (
     <View className={`space-y-2 ${otherStyles}`}>
       <Text className="text-primary font-ebInter text-xs">{title}</Text>
       <View className="w-full min-h-[54px] px-4 border-[1px] border-[#cfcfcf] rounded-md focus:border-black flex flex-row items-center">
-        <TextInput className={`flex-1 font-rInter ${amountInput}`}
+        <TextInput className={`flex-1 font-rInter`}
         // value={value}
         placeholder={placeholder}
         placeholderTextColor="#cfcfcf"
